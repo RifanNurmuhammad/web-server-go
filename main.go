0afc3dd16ed0ef03f7b03ed4bf983d12ebdecf21
rifan muhammad <rifan.rpl@gmail.com>
2019-02-14 23:02:25 +0700

    Initiate new router and unit testing

--- main.go
+++ main.go
@@ -9,10 +9,21 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func main() {
+// new router to initiate and test the router outside of the main function
+func newRouter() *mux.Router {
 	r := mux.NewRouter()
-
 	r.HandleFunc("/hello", handler).Methods("GET")
+
+	staticFileDirectory := http.Dir("./assets/")
+	staticFileHandler := http.StripPrefix("/assets/", http.FileServer(staticFileDirectory))
+	r.PathPrefix("/assets/").Handler(staticFileHandler).Methods("GET")
+
+	return r
+}
+
+func main() {
+	// calling `newRouter` constructor function
+	r := newRouter()
 	http.ListenAndServe(":8080", r)
 }
 
--- main_test.go
+++ main_test.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -33,9 +34,98 @@ func TestHandler(t *testing.T) {
 	}
 
 	// check the response body is what we expect.
-	expected := `Hello Go!`
+	expected := `Hello world`
 	actual := recorder.Body.String()
 	if actual != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v", actual, expected)
+	}
+}
+
+func TestRouter(t *testing.T) {
+	r := newRouter()
+
+	// create a new server using the 'httptest' libraries
+	mockServer := httptest.NewServer(r)
+
+	// make a GET request to the "hello" route we defined in the router
+	resp, err := http.Get(mockServer.URL + "/hello")
+
+	// handle any unexpected error
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// check status 200
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Status should be ok, got %d", resp.StatusCode)
+	}
+
+	// the response body is read
+	defer resp.Body.Close()
+
+	// read the body into a bunch of bytes (b)
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// convert the bytes to a string
+	respString := string(b)
+	expected := "Hello World!"
+
+	if respString != expected {
+		t.Errorf("Response should be %s, got %s", expected, respString)
+	}
+}
+
+func TestRouterForNonExistentRoute(t *testing.T) {
+	r := newRouter()
+	mockServer := httptest.NewServer(r)
+
+	// the code is similiar. The only difference is that now we make a request to route we know we didn't define
+	resp, err := http.Post(mockServer.URL+"/hello", "", nil)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// 405 method not allowed
+	if resp.StatusCode != http.StatusMethodNotAllowed {
+		t.Errorf("Status should be 405, got %d", resp.StatusCode)
+	}
+
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	respString := string(b)
+	expected := ""
+
+	if respString != expected {
+		t.Errorf("Response should be %s, got %s", expected, respString)
+	}
+}
+
+func TestStaticFileServer(t *testing.T) {
+	r := newRouter()
+	mockServer := httptest.NewServer(r)
+
+	// we want to hit the "GET /assets/" route to get the index.html
+	resp, err := http.GET(mockServer.URL + "/assets/")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Status should be 200, got %d", resp.StatusCode)
+	}
+
+	contentType := resp.Header.Get("Content-Type")
+	expectedContentType := "text/html; charset=utf-8"
+
+	if expectedContentType != contentType {
+		t.Errorf("Wrong content type, expected %s, got %s", expectedContentType, contentType)
 	}
 }
